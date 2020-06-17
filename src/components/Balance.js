00238da7c7c9a9663a3890328b517fa61a5350ed
yungalyx <52767711+yungalyx@users.noreply.github.com>
2020-06-16 22:42:18 -0700

    Update Balance.js
    yeah

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -10,7 +10,7 @@ export const Balance = () => {
     return (
         <>
             <h4>
-                Your Balance
+                Please Work
             </h4>
             <h1>${total}</h1>
         </>
